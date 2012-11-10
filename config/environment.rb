74ca38b1432650218245a7ad3ac41ddfa2833819
cmal <zyzy5730@163.com>
2012-11-10 19:43:49 +0800

    Task H: mailer failed to deliver a mail to the destination mailbox

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+# Task H: Sending Mail
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp # | :sendmail | :test
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.163.com",
+    port:                 25,
+    domain:               "Arch",
+    authetication:        :plain,
+    user_name:            "zyzy5730@163.com",
+    password:             "422912573000",
+    enable_starttls_auto: true
+  }
+end
